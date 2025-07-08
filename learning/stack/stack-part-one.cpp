bf3c3148ae2e8e975cabf6d118df1881df77ad36
Agleylson Melo <agleylsonmelo@outlook.com>
2025-07-08 11:12:08 -0300

    feat: enhance stack-part-one.cpp with improved stack status messages

--- learning/stack/stack-part-one.cpp
+++ learning/stack/stack-part-one.cpp
@@ -6,19 +6,24 @@ using namespace std;
 int main() {
 
 	stack<string> colors;
-
+	
 	colors.push("Red");
 	colors.push("Blue");
 	colors.push("Green");
 	colors.push("Orange");
 
+	if (colors.empty()) {
+		cout << "Stack is empty." << endl << endl;
+	} else {
+		cout << "Stack is not empty." << endl;
+	}
+
 	cout << "Stack size: " << colors.size() << endl;
-	cout << "Top element: " << colors.top() << endl;
+	cout << "Top element: " << colors.top() << endl << endl;
 	
-	colors.pop(); // Remove the top element (Orange)
-	
-	cout << "Stack size: " << colors.size() << endl;
-	cout << "Top element: " << colors.top() << endl;
+	while (!colors.empty()) {
+		colors.pop();
+	}
 
 	return 0;
 }
